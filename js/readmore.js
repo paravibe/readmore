27067688e5201bfa83e5681fdb53b7eb0f09bbd4
Maxym Petyurenko <drupalrv@gmail.com>
2013-06-11 19:28:17 +0300

    Issue #2012578: Read less

--- js/readmore.js
+++ js/readmore.js
@@ -7,6 +7,12 @@
         summary.hide();
         summary.next('.readmore-text').slideDown(300);
       });
+      $('.readmore-text .readless-link').click(function(e) {
+        e.preventDefault();
+        var text = $(this).closest('.readmore-text');
+        text.slideUp(300);
+        text.prev('.readmore-summary').slideDown(300);
+      });
     }
   };
 })(jQuery);
